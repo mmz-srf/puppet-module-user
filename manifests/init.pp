b28e3a502fe245c97f76e9b5f0e245e726982641
Marcel Haerry <haerry@puzzle.ch>
2008-06-27 10:00:18 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+#
+# user module
+#
+# Copyright 2008, Puzzle ITC
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
+
+# modules_dir { \"user\": }
+
+class user {
+    include user::base
+}
+
+class user::base {
+    package{'user':
+        ensure => present,
+    }
+    service{user:
+        ensure => running,
+        enable => true,
+        hasstatus => true,
+        require => Package[user],
+    }
+}
